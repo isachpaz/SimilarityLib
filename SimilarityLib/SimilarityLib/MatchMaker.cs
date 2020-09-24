7c802eb60aaaa12c5ac04e751ee1d96062865980
Ilias Sachpazidis <ilias.sachpazidis@gmail.com>
2020-09-24 16:14:47 +0200

    MatchMaker and MachMakerTests added

---
+++ SimilarityLib/SimilarityLib/MatchMaker.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace SimilarityLib
+{
+    public class MatchMaker
+    {
+        /// <summary>
+        /// Implementation based on http://people.cs.pitt.edu/~kirk/cs1501/Pruhs/Spring2006/assignments/editdistance/Levenshtein%20Distance.htm
+        /// </summary>
+        /// <param name="source">string-1</param>
+        /// <param name="target">string-2</param>
+        /// <returns></returns>
+        private static int ComputeLevenshteinDistance(string source, string target)
+        {
+            if (source == null || target == null){return 0;}
+            if (source.Length == 0 || target.Length == 0){return 0;}
+            if (source == target) {return source.Length;}
+
+            int sourceWordCount = source.Length;
+            int targetWordCount = target.Length;
+
+            if (sourceWordCount == 0)
+                return targetWordCount;
+
+            if (targetWordCount == 0)
+                return sourceWordCount;
+
+            int[,] distance = new int[sourceWordCount + 1, targetWordCount + 1];
+
+            for (int i = 0; i <= sourceWordCount; distance[i, 0] = i++) ;
+            for (int j = 0; j <= targetWordCount; distance[0, j] = j++) ;
+
+            for (int i = 1; i <= sourceWordCount; i++)
+            {
+                for (int j = 1; j <= targetWordCount; j++)
+                {
+                    int cost = (target[j - 1] == source[i - 1]) ? 0 : 1;
+                    distance[i, j] = Math.Min(Math.Min(distance[i - 1, j] + 1, distance[i, j - 1] + 1),
+                        distance[i - 1, j - 1] + cost);
+                }
+            }
+
+            return distance[sourceWordCount, targetWordCount];
+        }
+
+        /// <summary>
+        /// Calculates the similarity index of two strings: source, and target.
+        /// </summary>
+        /// <param name="source"></param>
+        /// <param name="target"></param>
+        /// <returns>double value from 0.0 to 1.0.</returns>
+        public static double CalculateSimilarityTwoStrings(string source, string target)
+        {
+            if (source == null || target == null){return 0.0;}
+            if (source.Length == 0 || target.Length == 0) {return 0.0;}
+            if (source == target) {return 1.0;}
+
+            var levenshteinDistance = ComputeLevenshteinDistance(source, target);
+            return 1.0 - (double) levenshteinDistance / (double) Math.Max(source.Length, target.Length);
+        }
+    }
+}
---
+++ SimilarityLib/SimilarityLibTests/MatchMakerTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using NUnit.Framework;
+using SimilarityLib;
+
+namespace SimilarityLibTests
+{
+    [TestFixture]
+    public class MatchMakerTests
+    {
+        [Test]
+        public void Similarity_Two_Equal_Strings_Test()
+        {
+            var similarityIndex = MatchMaker.CalculateSimilarityTwoStrings("medical innovation", "medical innovation");
+            Assert.AreEqual(1, similarityIndex);
+        }
+
+
+        [Test]
+        public void Similarity_Two_Semi_Equal_Strings_Test()
+        {
+            var similarityIndex = MatchMaker.CalculateSimilarityTwoStrings("medical innovation", "medical");
+            Assert.AreEqual(0.3889, Math.Round(similarityIndex, 4));
+        }
+
+        [Test]
+        public void Similarity_Two_Strings_with_Typing_Error_Test()
+        {
+            var similarityIndex = MatchMaker.CalculateSimilarityTwoStrings("medical", "medicla");
+            Assert.AreEqual(0.7143, Math.Round(similarityIndex, 4));
+        }
+
+        [Test]
+        public void Similarity_Two_Strings_Test()
+        {
+            var similarityIndex = MatchMaker.CalculateSimilarityTwoStrings("medical innovation", "and technology");
+            Assert.AreEqual(0.1667, Math.Round(similarityIndex, 4));
+        }
+    }
+}
